94f0818c0c37e26b9d493cd031835779fa283fce
AzStowe <aaron.z.stowe@gmail.com>
2019-06-04 15:12:44 -0500

    first commit

---
+++ shoppinglist.py
@@ -0,0 +1,38 @@
+# lets put it in the fridge
+
+print("working")
+
+shopping_list = []
+
+def show_help():
+    print("\n****What do you want to pick up?****")
+    print("""
+        Enter 'Done' to stop adding items.
+        Enter 'Show' to see your list of items.
+        """)
+def show_list():
+    #print out our list function
+    print("Here is what all you have in your shopping list")
+
+    for item in shopping_list:
+        print(item)
+
+def add_to_list(new_item):
+    # this function will add new items to the shopping list
+    shopping_list.append(new_item)
+    print("Added {}. Your shopping list now has {} item(s).".format(new_item, len(shopping_list)))
+
+
+show_help()
+
+while True:
+    #ask for new items
+    new_item = input(" > ")
+    #able to quit the app
+    if new_item == "Done":
+        show_list()
+        break
+    elif new_item == "Show":
+        show_list()
+        continue
+    add_to_list(new_item)
